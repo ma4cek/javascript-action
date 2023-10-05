a392c96d5cdb0c12865101d60323781f12df5c9f
Marek Vogel <marek.vogel@deutsche-boerse.com>
2023-10-05 15:12:25 +0200

    feat: changed log levels

--- dist/index.js
+++ dist/index.js
@@ -35,12 +35,12 @@ const core = __importStar(__nccwpck_require__(2186));
 try {
     // `who-to-greet` input defined in action metadata file
     const nameToGreet = core.getInput('who-to-greet');
-    core.notice(`Hello ${nameToGreet}!`);
+    core.info(`Hello ${nameToGreet}!`);
     const time = new Date().toTimeString();
     core.setOutput('time', time);
     // Get the JSON webhook payload for the event that triggered the workflow
     const payload = JSON.stringify(github.context.payload, undefined, 2);
-    core.notice(`The event payload: ${payload}`);
+    core.warning(`The event payload: ${payload}`);
 }
 catch (error) {
     if (error instanceof Error)
