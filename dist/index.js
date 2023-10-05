d6647567e3f34e66e5f86b4cff9e0a11f5788435
Marek Vogel <marek.vogel@deutsche-boerse.com>
2023-10-05 15:07:33 +0200

    feat: changed info level to notice

--- dist/index.js
+++ dist/index.js
@@ -35,12 +35,12 @@ const core = __importStar(__nccwpck_require__(2186));
 try {
     // `who-to-greet` input defined in action metadata file
     const nameToGreet = core.getInput('who-to-greet');
-    core.info(`Hello ${nameToGreet}!`);
+    core.notice(`Hello ${nameToGreet}!`);
     const time = new Date().toTimeString();
     core.setOutput('time', time);
     // Get the JSON webhook payload for the event that triggered the workflow
     const payload = JSON.stringify(github.context.payload, undefined, 2);
-    core.info(`The event payload: ${payload}`);
+    core.notice(`The event payload: ${payload}`);
 }
 catch (error) {
     if (error instanceof Error)
--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,12 @@ import * as core from '@actions/core'
 try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet')
-  core.info(`Hello ${nameToGreet}!`)
+  core.notice(`Hello ${nameToGreet}!`)
   const time = new Date().toTimeString()
   core.setOutput('time', time)
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
-  core.info(`The event payload: ${payload}`)
+  core.notice(`The event payload: ${payload}`)
 } catch (error) {
   if (error instanceof Error) core.setFailed(error.message)
 }
